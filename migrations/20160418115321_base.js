8e7cc5b95b26945ac6c241b471f2d707d6f97d98
Christian Amaral <darthcas@gmail.com>
2016-04-20 10:53:49 -0300

    there is no need to be upset

--- migrations/20160418115321_base.js
+++ migrations/20160418115321_base.js
@@ -19,47 +19,77 @@ exports.up = function (knex, Promise) {
       table.uuid('role').notNullable().index()
       table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
     })
+    .createTable('platform', function (table) {
+      table.string('id', 30).primary()
+      table.string('name', 30).notNullable()
+      table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
+    })
     .createTable('workspace_account', function (table) {
       table.uuid('id').primary()
+
       table.uuid('tetris_account').notNullable()
       table.string('external_account', 40).notNullable()
+
       table.uuid('workspace')
         .references('id')
         .inTable('workspace')
         .onDelete('cascade')
         .onUpdate('restrict')
         .notNullable()
+
+      table.string('external_name', 100).notNullable()
+      table.string('external_company_name', 100)
+
+      table.string('platform', 30)
+        .references('id')
+        .inTable('platform')
+        .onDelete('restrict')
+        .onUpdate('restrict')
+        .notNullable()
+
       table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
     })
     .createTable('folder', function (table) {
       table.uuid('id').primary()
       table.string('name', 50).notNullable()
-      table.uuid('workspace_account')
+
+      table.uuid('workspace')
         .references('id')
-        .inTable('workspace_account')
+        .inTable('workspace')
         .onDelete('cascade')
         .onUpdate('restrict')
         .notNullable()
+
       table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
     })
     .createTable('campaign', function (table) {
       table.uuid('id').primary()
       table.string('external_campaign', 40).notNullable()
+
       table.uuid('folder')
         .references('id')
         .inTable('folder')
         .onDelete('restrict')
         .onUpdate('restrict')
         .notNullable()
+
+      table.uuid('workspace_account')
+        .references('id')
+        .inTable('workspace_account')
+        .onDelete('restrict')
+        .onUpdate('restrict')
+        .notNullable()
+
       table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
     })
 }
 
 exports.down = function (knex, Promise) {
   return knex.schema
-    .dropTable('folder')
     .dropTable('campaign')
+    .dropTable('folder')
     .dropTable('workspace_account')
+    .dropTable('platform')
     .dropTable('workspace_role')
     .dropTable('workspace')
 }
