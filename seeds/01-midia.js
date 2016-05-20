f82c274079a97ba31183371918d4cd02bcb982cd
Christian Amaral <darthcas@gmail.com>
2016-05-20 14:51:20 -0300

    drops performance media

--- seeds/01-midia.js
+++ seeds/01-midia.js
@@ -2,7 +2,6 @@ exports.seed = function (knex, Promise) {
   return Promise.join(
     knex('media').del(),
     knex('media').insert({id: 'display', name: 'Display'}),
-    knex('media').insert({id: 'performance', name: 'Performance'}),
     knex('media').insert({id: 'search', name: 'Search'}),
     knex('media').insert({id: 'social', name: 'Social'}),
     knex('media').insert({id: 'video', name: 'Video'})
