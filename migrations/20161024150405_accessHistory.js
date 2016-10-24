dac4cf74852d2b58550e2f0d144fc1ac5b42f568
Christian Amaral <darthcas@gmail.com>
2016-10-24 16:55:48 -0200

    new access_history columns

--- migrations/20161024150405_accessHistory.js
+++ migrations/20161024150405_accessHistory.js
@@ -1,7 +1,37 @@
 exports.up = function (knex, Promise) {
-  return knex.schema
-    .createTable('access_history', function (table) {
+  return knex.schema.createTable('access_history', function (table) {
       table.uuid('company').notNullable()
+      table.uuid('user').notNullable()
+
+      table.uuid('workspace')
+        .references('id')
+        .inTable('workspace')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+
+      table.uuid('folder')
+        .references('id')
+        .inTable('folder')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+
+      table.uuid('order')
+        .references('id')
+        .inTable('order')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+
+      table.uuid('campaign')
+        .references('id')
+        .inTable('campaign')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+
+      table.uuid('report')
+        .references('id')
+        .inTable('report')
+        .onDelete('cascade')
+        .onUpdate('restrict')
 
       table.string('level', 20).notNullable()
       table.uuid('target').notNullable()
